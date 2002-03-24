6f6fd35398fc541e13f709592e7381fca575dd71
Christian Gosch <golib@goschs.de>
2002-03-24 16:33:07 +0000

    *** empty log message ***

---
+++ include/gonetobject.h
@@ -0,0 +1,33 @@
+/*
+ * This file and the programs contained in it and in associated files
+ * are copyright 2002 by Christian Gosch.
+ * Email: christian@goschs.de
+ * If no other license is supplied with this file, 
+ * assume it is distributable under the GNU General Public License (GPL).
+ */
+
+#ifndef GONETOBJECT_H
+#define GONETOBJECT_H
+
+#include <goserver.h>
+#include <gothreadobject.h>
+
+namespace goNet {
+
+class
+goNetObject : public goThreadObject
+{
+	public:
+		goNetObject(int listenPort);
+		virtual ~goNetObject();
+
+		virtual void threadMethod(); 
+	protected:
+			
+	private:
+		goServer server;
+};
+
+};
+#endif
+
