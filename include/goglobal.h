678f8d30750b1ea470100ba8c88f48d85910e6c2
Christian Gosch <golib@goschs.de>
2004-10-22 20:30:15 +0000

    global namespace

---
+++ include/goglobal.h
@@ -0,0 +1,11 @@
+#ifndef GOGLOBAL_H
+#define GOGLOBAL_H
+
+namespace goGlobal
+{
+    extern bool ILInitialized;       // Indicates if DevIL is
+                                     // initialized (if it is 
+                                     // used at all).
+}
+
+#endif
