e3572116fbfdad1cf5b4787444ce419fac4137d7
Christian Gosch <golib@goschs.de>
2004-12-18 17:16:52 +0000

    Shell script to make a tar.gz package

---
+++ package.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# Call this from one directory up from golib
+tar czvhf golib-`date +%Y%m%d`.tar.gz \
+ golib/AUTHORS\
+ golib/COPYING\
+ golib/ChangeLog\
+ golib/Doxyfile\
+ golib/INSTALL\
+ golib/Makefile.am\
+ golib/Makefile.in\
+ golib/NEWS\
+ golib/README\
+ golib/RELEASE\
+ golib/aclocal.m4\
+ golib/bootstrap\
+ golib/config.guess\
+ golib/config.status\
+ golib/config.sub\
+ golib/configure\
+ golib/configure.in\
+ golib/doc\
+ golib/examples\
+ golib/include\
+ golib/install-sh\
+ golib/libtool\
+ golib/ltmain.sh\
+ golib/maindocpage.h\
+ golib/missing\
+ golib/mkinstalldirs\
+ golib/namespacedoc.h\
+ golib/src\
+ golib/tools --exclude="graphics" --exclude=".svn" --exclude=".libs" --exclude=".deps" --exclude="*.o"
