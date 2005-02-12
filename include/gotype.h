d626b06a89a0aea5fe623b17b2022a4540aaae32
Christian Gosch <golib@goschs.de>
2005-02-12 21:50:34 +0000

    Added documentation

--- include/gotype.h
+++ include/gotype.h
@@ -9,7 +9,13 @@
 # include <goobjectbase.h>
 #endif
 
+/**
+ * @brief Comparison function (see goType).
+ **/
 typedef bool (*goCompareFunction)(const void*, const void*);
+/**
+ * @brief Index generating function (see goType).
+ **/
 typedef goIndex_t (*goIndexFunction)(const void*);
 
 // We need a function that:
@@ -25,6 +31,48 @@ class goTypePrivate;
  */
 /*! 
  * \brief Provides type information.
+ *
+ * This class contains information about a data type.
+ * It provides an ID enumerator (see goTypeEnum) such as
+ * GO_UINT8, GO_INT8, GO_UINT16, GO_INT16, GO_UINT32, GO_INT32, GO_FLOAT, GO_DOUBLE, ...,
+ * a character string describing the type, the data size in bytes,
+ * and a means to generate lookup tables for data.
+ * In order to create a lookup table, you can use
+ * getIndexFunction() to
+ * get a function which generates indices of type goIndex_t from a void* to
+ * a data value. You may also specify your own index generating functions for
+ * a specific type (see goIndexFunction).
+ * Get the minimum and maximum indices created by the function returned
+ * by getIndexFunction() using the methods getMinIndex() and getMaxIndex()
+ * and use these values to simply create an array of values you want to map
+ * each index to, like this:
+ *
+ * <pre>
+ *  ...
+ *  goSignal3D<void>* sig = ...;
+    goArray<goUInt16> LUT;
+    goUInt16*         LUTOrigin = 0;
+    {
+        goType targetType (GO_UINT16);
+        goIndex_t minIndex = sig->getDataType().getMinIndex();
+        goIndex_t maxIndex = sig->getDataType().getMaxIndex();
+        goDouble delta = (targetType.getMaximum() - targetType.getMinimum())/ (goDouble)(maxIndex - minIndex);
+        LUT.resize (maxIndex - minIndex + 1);
+        LUTOrigin = LUT.getPtr() - minIndex;
+        goIndex_t i;
+        goDouble value = (goDouble)targetType.getMinimum();
+        for (i = minIndex; i <= maxIndex; ++i)
+        {
+            LUTOrigin[i] = (goUInt16)value;
+            value += delta;
+        }
+    }
+    goIndexFunction indexFunction = sig->getDataType().getIndexFunction();
+    // Get the uint16 value for the data value at (5,1,2):
+    goUInt16 value = LUTOrigin[indexFunction(sig->getPtr(5,1,2)];
+    ...
+    </pre>
+ * 
  */
 class goType : public goObjectBase
 {
