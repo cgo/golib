9d364e2977e5fc2d8b8e81d293a4a237746cb431
Christian Gosch <golib@goschs.de>
2005-03-02 21:47:06 +0000

    Helper functions for signals

---
+++ include/gosignalhelper.h
@@ -0,0 +1,22 @@
+/*
+ * This file and the programs contained in it and in associated files
+ * are copyright 2003 by Christian Gosch.
+ * Email: christian@goschs.de
+ * If no other license is supplied with this file, 
+ * assume it is distributable under the GNU General Public License (GPL).
+ * $Id$
+ */
+
+#ifndef GOSIGNALHELPER_H
+#define GOSIGNALHELPER_H
+
+#ifndef GOSIGNAL3DBASE_H
+# include <gosignal3dbase.h>
+#endif
+#ifndef GOSIGNAL3D_H
+# include <gosignal3d.h>
+#endif
+
+bool goNormalizeSignal (const goSignal3DBase<void>* sig, goSignal3D<void>* targetSig);
+
+#endif
