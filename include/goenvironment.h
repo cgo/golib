d4588c891757efab785296dc3d018c43c679de77
Christian Gosch <golib@goschs.de>
2005-05-07 19:58:50 +0000

    System environment interface

---
+++ include/goenvironment.h
@@ -0,0 +1,15 @@
+/*
+ * This file and the programs contained in it and in associated files
+ * are copyright 2003 by Christian Gosch.
+ * Email: christian@goschs.de
+ * If no other license is supplied with this file, 
+ * assume it is distributable under the GNU General Public License (GPL).
+ * $Id$
+ */
+
+#ifndef GOENVIRONMENT_H
+#define GOENVIRONMENT_H
+
+bool goGetEnv (const char* variable, goString& retVal);
+
+#endif
