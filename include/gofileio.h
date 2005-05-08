856577f22f368ae202e151dbe17dabcc6458d850
Christian Gosch <golib@goschs.de>
2005-05-08 13:51:11 +0000

    ADded fileSize readAscii writeAscii fileExists

--- include/gofileio.h
+++ include/gofileio.h
@@ -6,8 +6,9 @@
 #endif
 
 #include <gotypes.h>
-#include <gosignal2d.h>
 #include <gostring.h>
+
+class goObjectBase;
 
 /*!
  * \addtogroup misc
@@ -24,20 +25,21 @@ class goFileIO {
  public:
   /*!
    * Reads a PGM (8 bit raw) file into the signal. Signal is newly created.
+   * \todo Fix readPGM etc. for signal3d
    */
-  static void readPGM (goString& filename, goSignal2D<goInt32>*& signal);
+//  static void readPGM (goString& filename, goSignal2D<goInt32>*& signal);
   ///
-  static void readPGM (const char* filename, goSignal2D<goInt32>*& signal);
+//  static void readPGM (const char* filename, goSignal2D<goInt32>*& signal);
 
   ///
-  static void readJPEG (const char* filename, goSignal2D<goInt32>*& signal);
+//  static void readJPEG (const char* filename, goSignal2D<goInt32>*& signal);
   /*!
    * Writes the signal as 8 bit raw PGM file. The signal should contain only values 
    * up to 255, since the values are simply casted to goInt8.
    */
-  static void writePGM (goString& filename, goSignal2D<goInt32>& signal);
+//  static void writePGM (goString& filename, goSignal2D<goInt32>& signal);
   ///
-  static void writePGM (const char* filename, goSignal2D<goInt32>& signal);
+//  static void writePGM (const char* filename, goSignal2D<goInt32>& signal);
 
   /** @addtogroup signal */
   /** @{ */
@@ -46,9 +48,12 @@ class goFileIO {
   /** @} */
   static FILE* createTempFile (goString& filenameRet);
   static bool  remove         (const goString& filename);
+  static goSize_t fileSize    (const char* filename);
+  static bool  readASCII      (const char* filename, goString& target);
+  static bool  writeASCII     (const char* filename, const goString& str);
+  static bool  fileExists     (const char* filename);
 };
 /*!
  * @}
  */
-
 #endif
