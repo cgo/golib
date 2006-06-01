ce97a141d73606ddb4865f2a89c94b7778e31f3e
Christian Gosch <golib@goschs.de>
2006-06-01 20:59:09 +0000

    Example for goMatlab

---
+++ examples/matlab/matlab.cpp
@@ -0,0 +1,22 @@
+#define HAVE_MATLAB 1
+
+#include <gomatlab.h>
+#include <stdlib.h>
+
+int main (int argc, char* argv[])
+{
+    goMatlab matlab;
+
+    matlab.doubleToVariable (1.0, "my_variable");
+    goString result;
+    result.resize(1024);
+    if (matlab.matlabCall ("my_variable", &result))
+    {
+        printf ("Ok, result: %s\n", result.toCharPtr());
+    }
+    else
+    {
+        printf ("Failure.\n");
+    }
+    exit(1);
+}
