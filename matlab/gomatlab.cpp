48189ef2234c67d08fb14b6b599a7368265439fe
Christian Gosch <golib@goschs.de>
2006-06-18 16:30:05 +0000

    removed -nosplash from command line

--- matlab/gomatlab.cpp
+++ matlab/gomatlab.cpp
@@ -254,7 +254,7 @@ bool goMatlab::startEngine ()
 {
     if (myPrivate->matlabEngine)
         this->stopEngine();
-    myPrivate->matlabEngine = engOpen ("matlab -nodesktop -nojvm");
+    myPrivate->matlabEngine = engOpen ("matlab -nodesktop -nosplash -nojvm");
     if (!myPrivate->matlabEngine)
     {
         printf ("Could not open matlab engine.\n");
