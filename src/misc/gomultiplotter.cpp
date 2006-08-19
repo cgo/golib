273c551bb48a45208034a66e2d0519109d2f2f75
Christian Gosch <golib@goschs.de>
2006-08-19 08:00:10 +0000

    Multiple plots in one window

---
+++ src/misc/gomultiplotter.cpp
@@ -0,0 +1,279 @@
+#include <goplot.h>
+#include <godefs.h>
+#include <gofileio.h>
+
+class goMultiPlotterPrivate
+{
+    public:
+        goMultiPlotterPrivate ()
+          : prefixCommands(), shellPostfix(), waitFlag(true), 
+            pauseFlag(false), cmdFilename(), 
+            plots() {};
+        ~goMultiPlotterPrivate () {};
+
+
+        goString               prefixCommands;
+        goString               shellPostfix;
+
+        bool              waitFlag;
+        bool              pauseFlag;
+        goString          cmdFilename;
+        goList<goSinglePlot> plots;
+
+        goSize_t rows;
+        goSize_t columns;
+};
+
+/** 
+ * @brief Constructor.
+ * 
+ * Constructs a new goMultiPlotter with multiplot grid size (rows x cols).
+ * This means a grid with <rows> rows and <cols> columns of goSinglePlot objects can be drawn by
+ * the constructed object.
+ *
+ * @param rows Number of rows.
+ * @param cols Number of columns.
+ */
+goMultiPlotter::goMultiPlotter (goSize_t rows, goSize_t cols)
+    : goObjectBase (), myPrivate (0)
+{
+    this->setClassID (GO_MULTIPLOTTER);
+    myPrivate = new goMultiPlotterPrivate;
+    myPrivate->rows = rows;
+    myPrivate->columns = cols;
+}
+
+goMultiPlotter::~goMultiPlotter ()
+{
+    if (myPrivate)
+    {
+        delete myPrivate;
+        myPrivate = 0;
+    }
+}
+
+goMultiPlotter::goMultiPlotter (goMultiPlotter& other)
+    : goObjectBase (), myPrivate (0)
+{
+    this->setClassID (GO_MULTIPLOTTER);
+    myPrivate = new goMultiPlotterPrivate;
+    *this = other;
+}
+
+goMultiPlotter& goMultiPlotter::operator= (goMultiPlotter& other)
+{
+    *this->myPrivate = *other.myPrivate;
+    return *this;
+}
+
+/** 
+ * @brief Add a plot at a given position.
+ * 
+ * Adds a plot to the multiplot grid.
+ *
+ * @param p     Plot.
+ * @param row   Row position.
+ * @param col   Column position.
+ */
+void goMultiPlotter::addPlot (const goSinglePlot& p, goSize_t row, goSize_t col)
+{
+    myPrivate->plots.append (p);
+    myPrivate->plots.getTailElement()->elem.setPosition (row, col);
+}
+
+/** 
+ * @brief Get number of rows in the multiplot grid.
+ * 
+ * @return Number of rows.
+ */
+goSize_t goMultiPlotter::getRows () const
+{
+    return myPrivate->rows;
+}
+
+/** 
+ * @brief Get number of columns in the multiplot grid.
+ * 
+ * @return Number of columns.
+ */
+goSize_t goMultiPlotter::getColumns () const
+{
+    return myPrivate->columns;
+}
+
+/** 
+ * @brief Set wait flag (default true).
+ * 
+ * If true, the caller waits for the gnuplot process.
+ *
+ * @param w Wait flag.
+ */
+void goMultiPlotter::setWaitFlag (bool w)
+{
+    myPrivate->waitFlag = w;
+}
+
+/** 
+ * @brief Get wait flag.
+ * 
+ * @return Wait flag.
+ */
+bool goMultiPlotter::getWaitFlag () const
+{
+    return myPrivate->waitFlag;
+}
+
+/** 
+ * @brief Set pause flag.
+ *
+ * If set to true, the plotter (gnuplot) waits for user input (usually <RET> on the console)
+ * until the program is resumed. To be precise, the gnuplot command "pause -1" is added at the end of the
+ * gnuplot command.
+ * 
+ * @param w Pause flag.
+ */
+void goMultiPlotter::setPauseFlag (bool w)
+{
+    myPrivate->pauseFlag = w;
+}
+
+/** 
+ * @brief Get pause flag.
+ * 
+ * @return Pause flag.
+ */
+bool goMultiPlotter::getPauseFlag () const
+{
+    return myPrivate->pauseFlag;
+}
+
+/** 
+ * @brief Set a gnuplot command prefix.
+ * 
+ * This string will be at the beginning of the gnuplot command.
+ * You can use this e.g. to set things like "unset xtics\nunset ytics\n".
+ * @note The string should be ended with a "\n" carriage return.
+ *
+ * @param p Command string.
+ */
+void goMultiPlotter::setPrefix (const goString& p)
+{
+    myPrivate->prefixCommands = p;
+}
+
+/** 
+ * @brief Plot all plots.
+ * 
+ * @return True if successful, false otherwise.
+ */
+bool goMultiPlotter::plot ()
+{
+    assert (!myPrivate->plots.isClosed());
+
+    goDouble stepX = 1.0 / static_cast<goDouble>(this->getColumns());
+    goDouble stepY = -1.0 / static_cast<goDouble>(this->getRows());
+
+    goString prefix = myPrivate->prefixCommands;
+    prefix += "set multiplot\nset size ";
+    prefix += (float)stepX;
+    prefix += ",";
+    prefix += (float)-stepY;
+    prefix += "\n";
+    goDouble posX  = 0.0;
+    goDouble posY  = 1.0 + stepY;
+
+    goString plotCommands = prefix;
+    goList<goSinglePlot>::Element *el = myPrivate->plots.getFrontElement();
+    while (el)
+    {
+        plotCommands += "set origin ";
+        plotCommands += (float)(posX + el->elem.getColumn() * stepX);
+        plotCommands += ",";
+        plotCommands += (float)(posY + el->elem.getRow() * stepY);
+        plotCommands += "\n";
+        el->elem.makePlot (plotCommands);
+        el = el->next;
+    }
+    
+    goString postfix = "unset multiplot\n";
+    if (myPrivate->pauseFlag)
+    {
+        postfix += "pause -1\n";
+    }
+    plotCommands += postfix;
+    bool ok = goPlot::callGnuplot (plotCommands, 
+                                myPrivate->shellPostfix != "" ? myPrivate->shellPostfix.toCharPtr() : 0, 
+                                myPrivate->waitFlag, 
+                                &myPrivate->cmdFilename);
+    //= If the process was paused, we can delete the files.
+    if (myPrivate->pauseFlag)
+    {
+        el = myPrivate->plots.getFrontElement();
+        while (el)
+        {
+            el->elem.removeFiles();
+            el = el->next;
+        }
+        goFileIO::remove (myPrivate->cmdFilename);
+    }
+
+    return ok;
+}
+
+/** 
+ * @brief Plot to a postscript file.
+ * 
+ * @param filename Postscript file name.
+ * 
+ * @return True if successful, false otherwise.
+ */
+bool goMultiPlotter::plotPostscript (const goString& filename)
+{
+    return this->plotFile (filename, goString("postscript color"));
+}
+
+/** 
+ * @brief Plot to a specified output terminal and pipe output to file.
+ * 
+ * The command "set terminal <type>" is added to the prefix gnuplot command.
+ * The plotter (gnuplot) is called through a shell and the shell output is
+ * stored in <filename>.
+ *
+ * @param filename Filename to store the shell output in.
+ * @param type     Terminal type (from gnuplot, e.g. "postscript color").
+ * 
+ * @return True if successful, false otherwise.
+ */
+bool goMultiPlotter::plotFile (const goString& filename, 
+                               const goString& type)
+{
+    goString backup1 = myPrivate->prefixCommands;
+    myPrivate->prefixCommands += "set terminal ";
+    myPrivate->prefixCommands += type;
+    myPrivate->prefixCommands += "\n";
+    goString backup2 = myPrivate->shellPostfix;
+    myPrivate->shellPostfix = " > ";
+    myPrivate->shellPostfix += filename;
+    bool ok = this->plot();
+    myPrivate->prefixCommands = backup1;
+    myPrivate->shellPostfix = backup2;
+    return ok;
+}
+
+/** 
+ * @brief Clears all commands and plots and resets flags to default values.
+ */
+void goMultiPlotter::clear ()
+{
+    myPrivate->prefixCommands = "";
+    myPrivate->shellPostfix = "";
+    myPrivate->waitFlag = true;
+    myPrivate->pauseFlag = false;
+    myPrivate->cmdFilename = "";
+    myPrivate->plots.erase ();
+}
+
+#ifndef GOLIST_HPP
+# include <golist.hpp>
+#endif
+template class goList<goSinglePlot>;
---
+++ src/misc/gosingleplot.cpp
@@ -0,0 +1,250 @@
+#include <goplot.h>
+#include <gofileio.h>
+
+class goSinglePlotPrivate
+{
+    public:
+        goSinglePlotPrivate() 
+            : plotX(), 
+              plotY(), 
+              titles(), 
+              plotCommands(), 
+              prefixCommands(""), 
+              dataFilenames(),
+              row(0),
+              column(0)
+        {};
+        ~goSinglePlotPrivate() {};
+
+        goList<goVectord>      plotX;
+        goList<goVectord>      plotY;
+        goList<goString>       titles;
+        goList<goPlotterLabel> labels;
+        goList<goString>       plotCommands;
+        goString               prefixCommands;
+
+        goList<goString>  dataFilenames;
+
+        goSize_t          row;
+        goSize_t          column;
+};
+
+goSinglePlot::goSinglePlot ()
+    : goObjectBase (), myPrivate (0)
+{
+    this->setClassID (GO_SINGLEPLOT);
+    myPrivate = new goSinglePlotPrivate;
+}
+
+goSinglePlot::~goSinglePlot ()
+{
+    if (myPrivate)
+    {
+        delete myPrivate;
+        myPrivate = 0;
+    }
+}
+
+goSinglePlot::goSinglePlot (const goSinglePlot& other)
+    : goObjectBase (), myPrivate (0)
+{
+    this->setClassID (GO_SINGLEPLOT);
+    myPrivate = new goSinglePlotPrivate;
+    *this = other;
+}
+
+const goSinglePlot& goSinglePlot::operator= (const goSinglePlot& other)
+{
+    *this->myPrivate = *other.myPrivate;
+    return *this;
+}
+
+/** 
+ * @brief Returns false. Comparison doesn't make sense.
+ * 
+ * @return false
+ */
+bool goSinglePlot::operator== (const goSinglePlot&) const
+{
+    return false;
+}
+
+bool goSinglePlot::operator!= (const goSinglePlot& other) const
+{
+    return !(*this == other);
+}
+
+/** 
+ * @brief Set position in multiplot.
+ * 
+ * Sets the position in the goMultiPlotter grid.
+ * The user does not have to set this, it is set with goMultiPlotter::addPlot().
+ *
+ * @param row Row
+ * @param col Column
+ */
+void goSinglePlot::setPosition (goSize_t row, goSize_t col)
+{
+    myPrivate->row = row;
+    myPrivate->column = col;
+}
+
+/** 
+ * @brief Get row position in the multiplot grid.
+ * 
+ * @return Row position.
+ */
+goSize_t goSinglePlot::getRow () const
+{
+    return myPrivate->row;
+}
+
+/** 
+ * @brief Get column position in the multiplot grid.
+ * 
+ * @return Column position.
+ */
+goSize_t goSinglePlot::getColumn () const
+{
+    return myPrivate->column;
+}
+
+/** 
+ * @brief Add a curve to the plot.
+ * 
+ * @param x X coordinates of curve points.
+ * @param y Y coordinates of curve points.
+ * @param title Title of the curve (set to "" for none).
+ * @param plotOptions Options for gnuplot, written after the plot command, default is "w l" for "with lines". E.g. set this to
+ * "with linespoints" for drawing with points and lines.
+ * 
+ * @return True if successful, false otherwise.
+ */
+bool goSinglePlot::addCurve (const goVectord& x, const goVectord& y, const char* title, const char* plotOptions)
+{
+    if (x.getSize() != y.getSize())
+    {
+        goLog::warning("addCurve(): x and y array sizes mismatch.",this);
+        return false;
+    }
+    myPrivate->plotX.append(x);
+    myPrivate->plotY.append(y);
+    myPrivate->titles.append(goString(title));
+    if (!plotOptions)
+    {
+        myPrivate->plotCommands.append(goString("w l"));
+    }
+    else
+    {
+        myPrivate->plotCommands.append(goString(plotOptions));
+    }
+    return true;
+}
+
+/** 
+ * @brief Add a label at a given position.
+ * 
+ * @param l Label text.
+ * @param x Position X
+ * @param y Position Y
+ * 
+ * @return True if successful, false otherwise.
+ */
+bool goSinglePlot::addLabel (const goString& l, goDouble x, goDouble y)
+{
+    return myPrivate->labels.append (goPlotterLabel(l.toCharPtr(), x, y));
+}
+
+/** 
+ * @brief Create all necessary temporary files and create gnuplot command string.
+ * 
+ * @param plotCommandsRet Contains the command string after the function returned true.
+ * 
+ * @return True if successful, false otherwise.
+ */
+bool goSinglePlot::makePlot (goString& plotCommandsRet) const
+{
+    goString prefix = myPrivate->prefixCommands;
+    if (!myPrivate->labels.isEmpty())
+    {
+        assert (!myPrivate->labels.isClosed());
+        goList<goPlotterLabel>::Element* el = myPrivate->labels.getFrontElement();
+        goSize_t labelTag = 0;
+        while (el)
+        {
+            prefix += "set label";
+            prefix += (int)labelTag;
+            prefix += " \"";
+            prefix += el->elem.label;
+            prefix += "\" at ";
+            prefix += (float)el->elem.x;
+            prefix += ",";
+            prefix += (float)el->elem.y;
+            prefix += "\n";
+            el = el->next;
+            ++labelTag;
+        }
+        // prefix += "replot\n";
+    }
+    goString postfix = "";
+
+    //= Unset labels
+    {
+        goIndex_t labelTag = 0;
+        for (labelTag = 0; labelTag < myPrivate->labels.getSize(); ++labelTag)
+        {
+            postfix += "unset label ";
+            postfix += (int)labelTag;
+            postfix += "\n";
+        }
+    }
+
+    if (!goPlot::writeGnuplotDataFiles (&myPrivate->plotX,
+                                        &myPrivate->plotY,
+                                        myPrivate->dataFilenames))
+    {
+        return false;
+    }
+    if (!goPlot::addGnuplotCommands (plotCommandsRet,
+                                     &myPrivate->dataFilenames,
+                                     &myPrivate->titles,
+                                     &myPrivate->plotCommands,
+                                     prefix.toCharPtr(),
+                                     postfix.toCharPtr()))
+    {
+        return false;
+    }
+    return true;
+}
+
+/** 
+ * @brief Removes temporary files.
+ *
+ * This is only safe to use when the gnuplot command has returned.
+ * goMultiPlotter copies the single plots, so the user will usually not have to deal with this.
+ * If the plot is not paused for user input after plotting (goMultiPlotter::setPauseFlag()), 
+ * the files are not deleted automatically and are left for a system service to clean (usually in /tmp).
+ */
+void goSinglePlot::removeFiles () const
+{
+    goList<goString>::ConstElement* el = myPrivate->dataFilenames.getFrontElement();
+    while (el)
+    {
+        goFileIO::remove (el->elem);
+        el = el->next;
+    }
+}
+
+/** 
+ * @brief Clears all curves.
+ */
+void goSinglePlot::clear ()
+{
+    myPrivate->plotX.erase ();
+    myPrivate->plotY.erase ();
+    myPrivate->titles.erase ();
+    myPrivate->labels.erase ();
+    myPrivate->plotCommands.erase ();
+    myPrivate->prefixCommands = "";
+    myPrivate->dataFilenames.erase ();
+}
