4e9f94bdd1258e7239b1ac49d0e8696671b73fa1
Christian Gosch <golib@goschs.de>
2006-09-23 09:59:04 +0000

    Added documentation; added size check before resize

--- src/math/divergence.cpp
+++ src/math/divergence.cpp
@@ -18,6 +18,25 @@ static bool divergence_ (const goSignal3DBase<void>& x, const goSignal3DBase<voi
     return true;
 }
 
+/** 
+ * @brief Calculate the divergence of a 2D vector field given by 2 goSignal3DBase.
+ * 
+ * Calculates \f$ D = \nabla V \f$, with V a two dimensional vector field over 
+ * real numbers, using central differences. So 
+ * \f$ D \approx \frac{\partial V}{\partial x} + \frac{\partial V}{\partial y} \f$
+ *
+ * The input must be of data type GO_FLOAT or GO_DOUBLE and
+ * both components must be of the same data type and size.
+ *
+ * @param x X components of the vector field.
+ * @param y Y components of the vector field.
+ * @param hx  Grid spacing in X direction (set this to 1.0 for now).
+ * @param hy  Grid spacing in Y direction (set this to 1.0 for now).
+ * @param retValue  Divergence, will be allocated to the same data type 
+ *   and size as the input data only if necessary.
+ * 
+ * @return True if successful, false otherwise.
+ */
 bool goMath::divergence (const goSignal3DBase<void>& x, const goSignal3DBase<void>& y, goDouble hx, goDouble hy, goSignal3D<void>& retValue)
 {
     if (x.getDataType().getID() != y.getDataType().getID())
@@ -31,10 +50,14 @@ bool goMath::divergence (const goSignal3DBase<void>& x, const goSignal3DBase<voi
         goLog::error ("goMath::divergence(): x and y must be of the same size.");
         return false;
     }
-    retValue.setDataType (x.getDataType().getID());
-    retValue.make (x.getSizeX(), x.getSizeY(), x.getSizeZ(),
-                   x.getBlockSizeX(), x.getBlockSizeY(), x.getBlockSizeZ(),
-                   4, 4, 4);
+    if (retValue.getSize() != x.getSize() ||
+        retValue.getDataType().getID() != x.getDataType().getID())
+    {
+        retValue.setDataType (x.getDataType().getID());
+        retValue.make (x.getSizeX(), x.getSizeY(), x.getSizeZ(),
+                x.getBlockSizeX(), x.getBlockSizeY(), x.getBlockSizeZ(),
+                4, 4, 4);
+    }
 
     switch (x.getDataType().getID())
     {
