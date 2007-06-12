55e78fdabd6d4fafc9d062305ecef7dcde3d1ccf
Christian Gosch <golib@goschs.de>
2007-06-12 21:03:30 +0000

    Added golapack.h -- declarations for standard lapack functions

---
+++ include/golapack.h
@@ -0,0 +1,16 @@
+#ifndef GOLAPACK_H
+#define GOLAPACK_H
+
+#include <f2c.h>
+
+extern "C" {
+/* Subroutine */ int sgesvd_(char *jobu, char *jobvt, integer *m, integer *n, 
+	real *a, integer *lda, real *s, real *u, integer *ldu, real *vt, 
+	integer *ldvt, real *work, integer *lwork, integer *info);
+/* Subroutine */ int dgesvd_(char *jobu, char *jobvt, integer *m, integer *n, 
+	doublereal *a, integer *lda, doublereal *s, doublereal *u, integer *
+	ldu, doublereal *vt, integer *ldvt, doublereal *work, integer *lwork, 
+	integer *info);
+}
+
+#endif
