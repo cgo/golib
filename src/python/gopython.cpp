a18d165550bd20f9a44fd4b80d63e5997bda0419
Christian Gosch <golib@goschs.de>
2007-08-29 20:55:49 +0000

    Does something useful now

--- src/python/gopython.cpp
+++ src/python/gopython.cpp
@@ -3,9 +3,11 @@
 class goPythonPrivate
 {
     public:
-        goPythonPrivate ()
+        goPythonPrivate () : mainMod(0), mainNamespace(0)
         {
             Py_Initialize (); //= should be a no-op if already initialised.
+            this->mainMod = PyImport_AddModule("__main__");
+            this->mainNamespace = PyModule_GetDict(mainMod);
         };
 
         ~goPythonPrivate ()
@@ -13,6 +15,9 @@ class goPythonPrivate
             Py_Finalize ();  //= FIXME Careful: add a reference count here
                              //= before destroying the interpreter.
         };
+
+        PyObject* mainMod;
+        PyObject* mainNamespace;
 };
 
 goPython::goPython ()
@@ -38,4 +43,65 @@ bool goPython::call (const goString& cmd)
 bool goPython::call (const char* cmd)
 {
     PyRun_SimpleString (cmd);
+}
+
+PyObject* goPython::run (const goString& cmd)
+{
+    PyObject* ret = PyRun_String (cmd.toCharPtr(), Py_file_input, myPrivate->mainNamespace, myPrivate->mainNamespace);
+    if (!ret)
+    {
+        PyErr_Print();
+    }
+}
+
+PyObject* goPython::getObject (const char* name)
+{
+    PyObject* tempName = PyString_FromString (name);
+    PyObject* ret = PyDict_GetItem (myPrivate->mainNamespace, tempName);
+    Py_XDECREF(tempName);
+    return ret;
+}
+
+goFloat goPython::getFloat (const char* name)
+{
+    PyObject* o = this->getObject (name);
+    goFloat ret = 0.0;
+    if (PyNumber_Check(o))
+    {
+        PyObject* f = PyNumber_Float(o);
+        ret = PyFloat_AsDouble(f);
+        Py_XDECREF(f);
+    }
+    return ret;
+}
+
+goDouble goPython::getDouble (const char* name)
+{
+    PyObject* o = this->getObject (name);
+    goDouble ret = 0.0;
+    if (PyNumber_Check(o))
+    {
+        PyObject* f = PyNumber_Float(o);
+        ret = PyFloat_AsDouble(f);
+        Py_XDECREF(f);
+    }
+    return ret;
+}
+
+void goPython::set (const char* name, goFloat f)
+{
+    PyObject* n = PyString_FromString (name);
+    PyObject* o = PyFloat_FromDouble (f);
+    PyDict_SetItem (myPrivate->mainNamespace, n, o);
+    Py_XDECREF(n);
+    Py_XDECREF(o);
+}
+
+void goPython::set (const char* name, goDouble f)
+{
+    PyObject* n = PyString_FromString (name);
+    PyObject* o = PyFloat_FromDouble (f);
+    PyDict_SetItem (myPrivate->mainNamespace, n, o);
+    Py_XDECREF(n);
+    Py_XDECREF(o);
 }
