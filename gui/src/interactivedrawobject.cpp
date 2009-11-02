e51fe7fb88326ffc6302675cfd52509369c5daed
Christian Gosch <golib@goschs.de>
2009-11-02 16:10:37 +0000

    Python fix in CMakeLists.txt files, now using FindPythonLibs; gui/src/interactivedrawobject.cpp: fixed namespace problem

--- gui/src/interactivedrawobject.cpp
+++ gui/src/interactivedrawobject.cpp
@@ -9,36 +9,39 @@ goGUI::InteractiveDrawObjectT<T>::InteractiveDrawObjectT (goAutoPtr<T> object_pt
     this->setObject (object_ptr);
 }
 
-template <>
-void goGUI::InteractiveDrawObjectT < goPlot::Object2DBox > ::setObject (goAutoPtr<goPlot::Object2DBox> object_ptr)
+namespace goGUI
 {
-    if (object_ptr)
-    {
-        this->getEditPoints().clear ();
-        goVectord p (2);
-        object_ptr->getCorners().refRow (0, p);
-        this->getEditPoints().push_back (p);
-        object_ptr->getCorners().refRow (1, p);
-        this->getEditPoints().push_back (p);
-    }
+    template <>
+        void InteractiveDrawObjectT < goPlot::Object2DBox > ::setObject (goAutoPtr<goPlot::Object2DBox> object_ptr)
+        {
+            if (object_ptr)
+            {
+                this->getEditPoints().clear ();
+                goVectord p (2);
+                object_ptr->getCorners().refRow (0, p);
+                this->getEditPoints().push_back (p);
+                object_ptr->getCorners().refRow (1, p);
+                this->getEditPoints().push_back (p);
+            }
 
-    myDrawObject = object_ptr;
-}
+            myDrawObject = object_ptr;
+        }
 
-template <>
-void goGUI::InteractiveDrawObjectT<goPlot::Object2DBox>::apply ()
-{
-    if (myDrawObject.isNull())
-        return;
+    template <>
+        void InteractiveDrawObjectT<goPlot::Object2DBox>::apply ()
+        {
+            if (myDrawObject.isNull())
+                return;
 
-    if (getEditPoints().size() < 2)
-        return;
+            if (getEditPoints().size() < 2)
+                return;
 
-    goVectord& p1 = *this->getEditPoints().begin();
-    goVectord& p2 = *(++this->getEditPoints().begin());
-    
-    myDrawObject->setCorners (p1[0], p1[1], p2[0], p2[1]);
-}
+            goVectord& p1 = *this->getEditPoints().begin();
+            goVectord& p2 = *(++this->getEditPoints().begin());
+
+            myDrawObject->setCorners (p1[0], p1[1], p2[0], p2[1]);
+        }
+};
 
 template <class T>
 goAutoPtr<T> goGUI::InteractiveDrawObjectT<T>::getObject ()
